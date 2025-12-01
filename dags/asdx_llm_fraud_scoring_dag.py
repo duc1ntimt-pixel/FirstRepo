649611d001ffe4bafef7a5327fc04f932def0363
IMT-SOFT\ducnt <ducnt@imt-soft.com>
2025-12-01 23:54:29 +0700

    + fix path

--- dags/asdx_llm_fraud_scoring_dag.py
+++ dags/asdx_llm_fraud_scoring_dag.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append("/opt/airflow/dags/tasks")
+sys.path.append("/opt/airflow/dags/repo/dags/tasks")
 from airflow import DAG
 from airflow.decorators import task
 from datetime import datetime, timedelta
