a9b0c7eaa8966ed83f288aa12c858e47eee53eb4
IMT-SOFT\ducnt <ducnt@imt-soft.com>
2025-12-19 11:47:48 +0700

    + new version

--- spark_pi_inline.py
+++ spark_pi_inline.py
@@ -1,32 +1,29 @@
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="spark_pi_inline",
+    dag_id="spark_pi_k8s",
     start_date=datetime(2025, 12, 18),
     schedule_interval=None,
     catchup=False,
+    tags=["spark", "kubernetes"],
 ) as dag:
 
-    spark_task = SparkKubernetesOperator(
+    spark_pi_task = SparkSubmitOperator(
         task_id="spark_pi",
-        namespace="spark-jobs",  
-        kubernetes_conn_id="kubernetes_default",
-        application_file={
-            "apiVersion": "sparkoperator.k8s.io/v1beta2",
-            "kind": "SparkApplication",
-            "metadata": {"name": "spark-pi", "namespace": "spark-jobs"},
-            "spec": {
-                "type": "Python",
-                "mode": "cluster",
-                "image": "spark:latest",  
-                "mainApplicationFile": "local:///opt/spark/examples/src/main/python/pi.py",
-                "sparkVersion": "3.5.0",
-                "restartPolicy": {"type": "Never"},
-                "driver": {"cores": 1, "memory": "512m", "serviceAccount": "spark"},
-                "executor": {"cores": 1, "memory": "512m", "instances": 1},
-            },
-        },
-        do_xcom_push=True,
+        application="local:///opt/spark/examples/src/main/python/pi.py",
+        conn_id="spark_default",
+        executor_cores=1,
+        executor_memory="512m",
+        driver_cores=1,
+        driver_memory="512m",
+        name="spark-pi-job",
+        verbose=True,
+        conf={
+            "spark.kubernetes.namespace": "spark-jobs",
+            "spark.kubernetes.container.image": "spark:latest",
+            "spark.submit.deployMode": "cluster",
+            "spark.kubernetes.authenticate.driver.serviceAccountName": "spark"
+        }
     )
