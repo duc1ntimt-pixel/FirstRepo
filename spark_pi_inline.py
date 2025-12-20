d8c65b08695ddf1ca86545e226ea221030299ad2
IMT-SOFT\ducnt <ducnt@imt-soft.com>
2025-12-20 14:14:06 +0700

    no message

--- spark_pi_inline.py
+++ spark_pi_inline.py
@@ -1,17 +1,29 @@
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 from datetime import datetime
+import time
+
+def wait_for_xcom(ti, task_id: str, timeout: int = 60):
+    waited = 0
+    while waited < timeout:
+        app = ti.xcom_pull(task_ids=task_id)
+        if app and 'metadata' in app and 'name' in app['metadata']:
+            return app['metadata']['name']
+        time.sleep(2)
+        waited += 2
+    raise ValueError(f"XCom from {task_id} not found after {timeout}s")
 
 with DAG(
-    dag_id="spark_pi_operator",
+    dag_id="spark_pi_dynamic_sensor",
     start_date=datetime(2025, 12, 18),
     schedule=None,
     catchup=False,
     tags=["spark", "kubernetes"],
 ) as dag:
 
-    spark_pi_submit = SparkKubernetesOperator(
+    spark_submit = SparkKubernetesOperator(
         task_id="spark_pi_submit",
         namespace="spark-jobs",
         application_file="spark-pi.yaml",
@@ -19,11 +31,18 @@ with DAG(
         get_logs=True,
     )
 
-    spark_pi_sensor = SparkKubernetesSensor(
-        task_id="spark_pi_sensor",
-        namespace="spark-jobs",
-        application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
-        poke_interval=10,
+    wait_xcom = PythonOperator(
+        task_id="wait_for_xcom",
+        python_callable=wait_for_xcom,
+        op_kwargs={"task_id": "spark_pi_submit", "timeout": 120},
     )
 
-    spark_pi_submit >> spark_pi_sensor
+    spark_sensor = SparkKubernetesSensor(
+        task_id="spark_pi_sensor",
+        namespace="spark-jobs",
+        application_name="{{ ti.xcom_pull(task_ids='wait_for_xcom') }}",
+        poke_interval=10,
+        timeout=600,
+    )
+
+    spark_submit >> wait_xcom >> spark_sensor
